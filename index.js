29aa24cae7f92f4c5f2dbd08ade33f781aa85c72
小鳥遊銀 <48020662+TakanashiGin@users.noreply.github.com>
2023-01-06 02:30:10 +0000

    hitomazu

--- index.js
+++ index.js
@@ -8,6 +8,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index.ejs'));
+const system = {
+    static: {
+        interval_time: 5000,
+        timeout_time: 3
+    },
+    variable: {
+        connected: false,
+        connected_time: null
+    },
+    getTime: function(){
+        const date_object = new Date();
+        return {
+            h: date_object.getHours(),
+            m: date_object.getMinutes(),
+        }
+    }
+}
+
+app.get('/', (req, res) => res.render('index.ejs', {
+    title: "AutomaticFireExtinguisherDetectionHerokuSystem",
+    connected_client: system.variable.connected
+}));
+
+app.post('/afe', (req, res) => {
+    system.variable.connected_time = system.getTime();
+    systeme.variable.connected = true;
+    console.log(`[connected client] Hours: ${system.variable.connected_time.h}, Minutes: ${system.variable.connected_time.m}`);
+});
+
+setInterval(() => {
+    if (system.variable.connected) {
+        const connected_time = system.variable.connected_time;
+        const now_time = system.getTime(); 
+        if ((now_time.m - connected_time.m >= system.static.timeout_time) || (now_time.m - connected_time.m < system.static.timeout_time && now_time.h - connected_time.h >= 1)) {
+            system.variable.connected = false;
+            system.variable.connected_time = null;
+        }
+    }
+}, system.static.interval_time);
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ readme.md
@@ -0,0 +1,14 @@
+# AutomaticFireExtinguisherDetectionHerokuSystem for Render
+
+render url:
+https://dashboard.render.com/
+
+setup
+```
+$ npm install express ejs
+```
+
+run
+```
+$ node ./index.js
+```
